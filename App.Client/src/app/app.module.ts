7ddd5fa640680c2a82645149f68998cfec129940
Joao Marques <joao.marques@gclaims.com.br>
2024-04-22 16:07:00 -0300

    Adição do menu e do footer

--- App.Client/src/app/app.module.ts
+++ App.Client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LogsComponent } from './logs/logs.component';
+import { MainComponent } from './main/main.component';
+import { MenuComponent } from './main/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogsComponent
+    LogsComponent,
+    MainComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
